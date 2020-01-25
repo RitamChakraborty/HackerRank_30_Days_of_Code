01385ba7ad3a1be80c12e6e28337fe1e239c9a2b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-01-25 17:07:52 +0530

    Solve Day 5: Loops and Day 6: Let's Review

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - [Day 2: Operators](https://www.hackerrank.com/challenges/30-operators/problem)
 - [Day 3: Intro to Conditional Statements](https://www.hackerrank.com/challenges/30-conditional-statements/problem)
 - [Day 4: Class vs. Instance](https://www.hackerrank.com/challenges/30-class-vs-instance/problem)
+- [Day 5: Loops](https://www.hackerrank.com/challenges/30-loops/problem)
+- [Day 6: Let's Review](https://www.hackerrank.com/challenges/30-review-loop/problem)
---
+++ src/day_6_lets_review/Solution.java
@@ -0,0 +1,28 @@
+package day_6_lets_review;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int t = scanner.nextInt();
+		
+		while (t-- > 0) {
+			char[] chars = scanner.next().toCharArray();
+			StringBuilder even = new StringBuilder();
+			StringBuilder odd = new StringBuilder();
+			
+			for (int i = 0; i < chars.length; ++i) {
+				if (i % 2 == 0) {
+					even.append(chars[i]);
+				} else {
+					odd.append(chars[i]);
+				}
+			}
+			
+			System.out.println(even.append(" ").append(odd).toString());
+		}
+	}
+}
