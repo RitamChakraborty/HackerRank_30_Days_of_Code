e9d65fc83619389cada65b5189a5a1e5f858128b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-01-27 23:04:20 +0530

    Solve Day 28: Regex, Patterns, and Intro to databases

---
+++ src/day_28_regex_patterns_and_intro_to_dabases/Solution.java
@@ -0,0 +1,26 @@
+package day_28_regex_patterns_and_intro_to_dabases;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int t = scanner.nextInt();
+		List<String> list = new ArrayList<>();
+		Pattern pattern = Pattern.compile("[.a-z]+@gmail.com");
+		
+		while (t-- > 0) {
+			String name = scanner.next();
+			String email = scanner.next();
+			
+			if (pattern.matcher(email).matches()) {
+				list.add(name);
+			}
+		}
+		
+		list.stream().sorted().forEach(System.out::println);
+	}
+}
